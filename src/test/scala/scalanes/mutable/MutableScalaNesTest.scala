aff67310c743ec7f2c2c69d0e38d391375bde8c4
mpod <mpod@fastmail.fm>
2020-10-04 23:39:43 +0200

    Test for mutable version

---
+++ src/test/scala/scalanes/mutable/MutableScalaNesTest.scala
@@ -0,0 +1,72 @@
+package scalanes.mutable
+
+import java.nio.file.Paths
+
+import cats.effect.concurrent.Ref
+import cats.effect.{Blocker, ContextShift, IO}
+import fs2.{io, text, Stream}
+import org.scalatest.Matchers
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.concurrent.ExecutionContext
+
+class MutableScalaNesTest extends AnyFlatSpec with Matchers {
+
+  implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+
+  def compare(logLine: String, s: NesState): Boolean = {
+    val op = Cpu.cpuRead(s.cpuState.pc).runA(s)
+    logLine.contains(s"${hex(s.cpuState.pc, 4)}  ${hex(op, 2)} ") &&
+    logLine.contains(s" A:${hex(s.cpuState.a, 2)}") &&
+    logLine.contains(s" X:${hex(s.cpuState.x, 2)}") &&
+    logLine.contains(s" Y:${hex(s.cpuState.y, 2)}") &&
+    (
+      logLine.contains(s" P:${hex(s.cpuState.status, 2)}") ||
+        logLine.contains(s" P:${hex(s.cpuState.status & 0xef, 2)}")
+    ) &&
+    logLine.contains(s" SP:${hex(s.cpuState.stkp, 2)}")
+  }
+
+  "Mutable ScalaNes" should "pass the nestest test" in {
+    val nestestRom = Paths.get(getClass.getResource("/nestest.nes").toURI)
+    val nestestLog = Paths.get(getClass.getResource("/nestest.log").toURI)
+
+    val decodedNesRom = (
+      for {
+        controller <- Ref.of[IO, UInt8](0x00)
+        loaded     <- NesState.fromFile[IO](nestestRom, controller).compile.toList
+      } yield loaded
+    ).unsafeRunSync()
+    decodedNesRom should have size 1
+    val initialNesState =
+      NesState.reset.flatMap(_ => State.get).map(nes => Cpu.setPc(0xc000)(nes)).runS(decodedNesRom.head)
+
+    var nesTestLog: List[String] = Stream
+      .resource(Blocker[IO])
+      .flatMap { blocker =>
+        io.file
+          .readAll[IO](nestestLog, blocker, 4096)
+          .through(text.utf8Decode)
+          .through(text.lines)
+      }
+      .compile
+      .toList
+      .unsafeRunSync()
+
+    var next = initialNesState
+    while (nesTestLog.nonEmpty && compare(nesTestLog.head, next)) {
+      next = Cpu.executeNextInstr.runS(next)
+      nesTestLog = nesTestLog.tail
+    }
+    if (nesTestLog.nonEmpty) {
+      val pc = hex(next.cpuState.pc, 4)
+      val op = Cpu.cpuRead(next.cpuState.pc).runA(next)
+      val a  = hex(next.cpuState.a, 2)
+      val x  = hex(next.cpuState.x, 2)
+      val y  = hex(next.cpuState.y, 2)
+      val p  = hex(next.cpuState.status, 2)
+      println(s"$pc  $op A:$a X:$x Y:$y P:$p   <-->   ${nesTestLog.head}")
+    }
+    nesTestLog shouldBe empty
+  }
+}
